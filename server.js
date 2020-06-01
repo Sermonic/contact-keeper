54b9f36c33e013717d89dce2cf70611a16089b62
Sermonic <vitaliyshcherbanych@gmail.com>
2020-06-01 11:52:09 +0300

    Setup React & Concurrently

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/contacts", require("./routes/contactRoutes"));
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
